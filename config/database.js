68b1459b1df43f740dc4a57d8fd8f7d2eef49bd0
arbhutale <tech1arb@gmail.com>
2021-08-12 04:39:10 +0530

    uated code

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 module.exports = {
     //uri:'mongodb://localhost:27017/mean-angular-2',
-    uri: 'mongodb://blog:arb1tech@ds149373.mlab.com:49373/blog',
+    uri: 'mongodb://blog:admin@cluster0-shard-00-00.xsyd5.mongodb.net:27017,cluster0-shard-00-01.xsyd5.mongodb.net:27017,cluster0-shard-00-02.xsyd5.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-106no0-shard-0&authSource=admin&retryWrites=true&w=majority',
     secret: crypto,
     db: 'blog',
 }
--- env.js
+++ env.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 if (fs.existsSync('./public')) {
   process.env.NODE_ENV = 'production';
-  process.env.databaseUri = 'mongodb://patel:patel@ds153752.mlab.com:53752/angular-2-app'; // Databse URI and database name
+  process.env.databaseUri = 'mongodb://blog:admin@cluster0-shard-00-00.xsyd5.mongodb.net:27017,cluster0-shard-00-01.xsyd5.mongodb.net:27017,cluster0-shard-00-02.xsyd5.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-106no0-shard-0&authSource=admin&retryWrites=true&w=majority'; // Databse URI and database name
   process.env.databaseName = 'production database: angular-2-app'; // Database name
 } else {
   process.env.NODE_ENV = 'development';
-  process.env.databaseUri = 'mongodb://localhost:27017/mean-angular-2'; // Databse URI and database name
+  process.env.databaseUri = 'mongodb://blog:admin@cluster0-shard-00-00.xsyd5.mongodb.net:27017,cluster0-shard-00-01.xsyd5.mongodb.net:27017,cluster0-shard-00-02.xsyd5.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-106no0-shard-0&authSource=admin&retryWrites=true&w=majority'; // Databse URI and database name
   process.env.databaseName = 'development database: mean-angular-2'; // Database name
 }
